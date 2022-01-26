b5c55c2b767591a8a79805d05dcbe310f3fb5d95
Stephen Cook <31995104+sjc5897@users.noreply.github.com>
2022-01-26 18:02:05 -0500

    Initial commit

---
+++ binarySearch/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace binarySearch
+{
+    class Program
+    {
+        public static int Search(int[] nums, int target) {
+            int left = 0;
+            int right = nums.Length - 1;
+        
+            while(left <= right){
+                int middle = left + (right - left) / 2;
+                if(nums[middle] == target){
+                    return middle;
+                }
+                else if(nums[middle] > target){
+                    right = middle - 1;
+                }
+                else{
+                    left = middle + 1;
+                }
+            }
+        
+            return -1;
+        
+        }
+        
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] {-1,0,3,5,9,12};
+            int target = 9;
+            
+            Console.WriteLine(Search(nums,target));
+        }
+    }
+}
