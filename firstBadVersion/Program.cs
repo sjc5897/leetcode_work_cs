c3e40f89becf089d85d305ca99ea264011e820cd
Stephen Cook <stevejcook114@gmail.com>
2022-01-27 12:26:18 -0500

    finished 2 assignments

---
+++ firstBadVersion/Program.cs
@@ -0,0 +1,16 @@
+﻿// https://leetcode.com/problems/first-bad-version/submissions/
+// public class FirstBadVersion{
+//     public int FirstBadVersion(int n) {
+//         int left = 0, right = n;
+//         while(left <= right){
+//             int middle = left + (right - left) / 2;
+//             if(IsBadVersion(middle)){
+//                 right = middle - 1;
+//             }
+//             else{
+//                 left = middle + 1;
+//             }
+//         }
+//         return left;
+//     }
+// }
---
+++ searchInsertPosition/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// https://leetcode.com/problems/search-insert-position/
+public class SearchInsertPosition{
+    public static int SearchInsert(int[] nums, int target)
+    {
+        int left = 0, right = nums.Length - 1;
+        while (left <= right)
+        {
+            int middle = left + (right - left) / 2;
+            if (nums[middle] == target)
+            {
+                return middle;
+            }
+
+            if (nums[middle] > target)
+            {
+                right = middle - 1;
+            }
+            else
+            {
+                left = middle + 1;
+            }
+        }
+
+        return right + 1;
+    }
+
+    public static void Main(String[] args)
+    {
+        int[] nums = new[] {1, 3, 5, 6};
+        int target = 5;
+        Console.Write(SearchInsert(nums,target));
+    }
+
+}
